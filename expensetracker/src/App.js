526fcfacdefde8ba24c1075bb9e8c7c89f0a3a18
SoumyaS1999 <soumya1999crj@gmail.com>
2024-03-16 18:33:58 +0530

    Initial commit

---
+++ expensetracker/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AuthForm from './components/Auth/Authform';
+
+function App() {
+  return (
+    <div>
+      <AuthForm />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ expensetracker/src/components/Auth/Authform.js
@@ -0,0 +1,107 @@
+import { useState, useRef , useContext} from "react";
+//import AuthContext from "../Store/auth-context";
+//import { Redirect } from 'react-router-dom';
+import classes from "./Authform.module.css";
+
+const AuthForm = () => {
+  //const history= useHistory()
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  //const authCtx = useContext(AuthContext);
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+        url="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAysHuvFXa07LIxOJi5WvSY-OSNV7r2FZA"
+    } 
+    else {
+        url="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAysHuvFXa07LIxOJi5WvSY-OSNV7r2FZA"
+    }
+    fetch( url,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "EMAIL_EXISTS!";
+            console.log(data);
+
+            //alert(errorMessage);
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data)=>{
+        //authCtx.login(data.idToken);
+        console.log(data);
+        alert('Login Successful')
+      })
+      .catch((err)=>{
+        alert(err.message);
+      });
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
